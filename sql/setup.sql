69ff8b58d54e8125199bf22df3514a3084bff139
cyrill <siril.taka@gmail.com>
2020-10-03 13:48:47 +0900

    Allow isucon user %

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `xsuportal` DEFAULT CHARACTER SET utf8mb4;
-CREATE USER IF NOT EXISTS `isucon`@`localhost` IDENTIFIED WITH mysql_native_password BY 'isucon';
-GRANT ALL ON `xsuportal`.* TO `isucon`@`localhost`;
+CREATE USER IF NOT EXISTS `isucon`@`%` IDENTIFIED WITH mysql_native_password BY 'isucon';
+GRANT ALL ON `xsuportal`.* TO `isucon`@`%`;
